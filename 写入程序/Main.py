e9d2fddf13fc4934038b6d3a8054636f0c8f9879
Angelagoodboy <2788549342@qq.com>
2020-04-26 23:06:24 +0800

    '4月26日提交'

--- 写入程序/Main.py
+++ 写入程序/Main.py
@@ -1,16 +1,20 @@
 from  tkinter import *
-import tkinter
+import tkinter,UI_Event
 import OpenFile
 from OpenFile import *
 from  tkinter  import ttk
 
 class  MainActivity(object):
-    global path
+    helloCommboxEvent=UI_Event.helloCommboxEvent
 
     def _init_(self):
         print('执行初始化方法')
     def callback(self):
-        openFile(self)
+        openbinFile(self)
+    def callback_combobox(self,value):
+        self.helloCommboxEvent.comboxselectEvent( self.helloCommboxEvent,value)
+    def callback_brwser(self):
+        self.helloCommboxEvent.btn_browse(self.helloCommboxEvent)
     def Main(self):
         global path
         path=tkinter.StringVar
@@ -22,17 +26,21 @@ class  MainActivity(object):
         root['menu'] = menu
         # menumenu.add_command(label='菜单')
         menu.add_cascade(label='打开文件', command=self.callback)
-        menu.bind('<Button-1>',open)
+
         label_carType = tkinter.Label(text="选择车型", font=('Arial', 10)).place(x=60, y=40, width=100, height=30)
         comvalue = tkinter.StringVar()  # 窗体自带的文本，新建一个值
         comboxlist = ttk.Combobox(root, textvariable=comvalue)  # 初始化
-        comboxlist["values"] = ("奥迪", "奔驰", "宝马", "雷克萨斯")
+        comboxlist["values"] = ("宝马STM32F105RBT6", "宝马8823", "8368奥迪高配", "8368奥迪低配","8368奥迪Q7换屏","8368雷克萨斯","8368宝马NBT","8368宝马CIC","8368奔驰1"
+                                                                                                                                     "8368奔驰2")
         comboxlist.current(0)  # 选择第一个
-        comboxlist.bind("<<ComboboxSelected>>", root)  # 绑定事件,(下拉列表框被选中时，绑定go()函数)
+        comboxlist.bind("<<ComboboxSelected>>", lambda event :self.callback_combobox(comboxlist.get()))
         comboxlist.place(x=160, y=40, width=100, height=30)
         label_path = tkinter.Label(text="输出路径", font=('Arial', 10)).place(x=60, y=80, width=100, height=30)
-        Entry_path = tkinter.Entry(root,textvariable=path).place(x=160,y=80,width=250,height=25)
-        btn_browse = tkinter.Button(root, text='浏览').place(x=420, y=80, width=80, height=25)
+        default_value = StringVar()
+        default_value.set('1')
+        Entry_path = tkinter.Entry(root,textvariable=default_value).place(x=160,y=80,width=250,height=25)
+        btn_browse = tkinter.Button(root, text='浏览',command=self.callback_brwser).place(x=420, y=80, width=80, height=25)
+
         btn_confirm=tkinter.Button(root,text='确认写入').place(x=150,y= 120,width=80,height=25)
         root.mainloop()  # 进入消息循环（必需组件）
 
--- 写入程序/OpenFile.py
+++ 写入程序/OpenFile.py
@@ -1,2 +1,29 @@
-def openFile(self):
-    print('open file')
+import  os
+from tkinter import filedialog
+global file_path
+global file_text
+
+def openbinFile():
+
+    '打开文件'
+
+    file_path = filedialog.askopenfilename(title='选择文件',initialdir=(os.path.expanduser('C:/')))
+    print('打开文件：', file_path)
+    return file_path
+
+
+
+def writeFile(file_path,data):
+    '写入文件'
+    tmp=bytes(1024)
+    if file_path is not None:
+        with open(file_path, mode='rb') as file:
+            #读出原来的内容
+            while True:
+                strb = file.read(1024)
+                if strb == b"":
+                    break
+                print(strb)
+                tmp=strb
+        file.write(data)
+        file.append(tmp)
